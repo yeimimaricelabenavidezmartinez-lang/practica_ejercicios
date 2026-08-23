f6ed32415e5caca004dfcaab53acd8c15918c1e8
Yeimi <yeimimaricelabenavidezmartinez@gmail.com>
2026-08-23 00:11:17 -0600

    Actualizando comentarios y formato

--- Pracica-01/ejercicio1.js
+++ Pracica-01/ejercicio1.js
@@ -3,15 +3,17 @@
 //(suma, resta, multiplicación, división y módulo) y muestre cada resultado por separado en
 //consola.
 
-
+//Paso 1: Declaracion de variables numericas para realizar las operaciones
 let num1 = 10;
 let num2 = 20;
 
+//Paso 2: Calculo de las operaciones aritmeticas requeridas
 let suma = num1 + num2;
 let resta = num1 - num2;
 let multiplicación = num1 * num2;
 let modulo = num2 % num1;
 
+//Paso 3: Impresion de los resultados en la consola con mensajes descriptivos
 console.log(`La suma es : ${suma}`);
 console.log(`La resta es: ${resta}`);
 console.log(`La multiplicion es: ${multiplicación}`);
--- Pracica-01/ejercicio5.js
+++ Pracica-01/ejercicio5.js
@@ -2,10 +2,13 @@
 //Declare tres variables con calificaciones numéricas. Calcule el promedio y muestre el
 //resultado.
 
+//Paso 1: Declarar las tres caliicaciones de las evaluaciones
 let nota1 = 10;
 let nota2 = 9;
 let nota3 =  8;
 
+//Paso 2: Calcular el promedio dividido con el total (3)
 let promedio = (nota1+nota2+nota3)/3
 
+//Paso 3: Mostrar los resultados detallados en la consola
 console.log(`Su promedio es: ${promedio}`)
